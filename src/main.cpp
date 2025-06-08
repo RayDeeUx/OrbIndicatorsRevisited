de49fcfd00b58e0f4a9d11e8144867d08aa628ff
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-08 14:21:04 -0400

    alright let's get this show on the road

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) return false;
+bool enabled = false;
 
-		return true;
+class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
+	void update(float dt) {
+		GJBaseGameLayer::update(dt);
+		if (!enabled || !m_orbIndicators || !m_indicatorSprites) return;
+		for (const auto indicatorSprite : CCArrayExt<CCSprite*>(m_indicatorSprites)) {
+			indicatorSprite->setScale(4.f);
+		}
 	}
 };
+
+$on_mod(Loaded) {
+	enabled = Mod::get()->getSettingValue<bool>("enabled");
+	listenForAllSettingChanges([](std::shared_ptr<SettingV3> _) {
+		enabled = Mod::get()->getSettingValue<bool>("enabled");
+	});
+}

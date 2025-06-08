fa4f484ce9092a1b591f39a22749704cef1b2d57
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-08 14:50:21 -0400

    thx @alphalaneous

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Geode/modify/GJBaseGameLayer.hpp>
 
+#define NOT_RED gameObject->m_objectType != GameObjectType::RedJumpRing
+#define NOT_PINK gameObject->m_objectType != GameObjectType::PinkJumpRing
+#define NOT_YELLOW gameObject->m_objectType != GameObjectType::YellowJumpRing
+#define NOT_GRAVITY gameObject->m_objectType != GameObjectType::GravityRing
+#define NOT_GRAVJUMP gameObject->m_objectType != GameObjectType::GreenRing
+#define NOT_BLACK_DROP gameObject->m_objectType != GameObjectType::DropRing
+
 using namespace geode::prelude;
 
 bool enabled = false;
@@ -9,6 +16,9 @@ class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
 		GJBaseGameLayer::update(dt);
 		if (!enabled || !m_orbIndicators || !m_indicatorSprites) return;
 		for (const auto indicatorSprite : CCArrayExt<CCSprite*>(m_indicatorSprites)) {
+			if (!indicatorSprite->getParent()) continue;
+			const auto gameObject = static_cast<GameObject*>(indicatorSprite->getParent());
+			if (NOT_RED && NOT_PINK && NOT_YELLOW && NOT_GRAVITY && NOT_GRAVJUMP && NOT_BLACK_DROP) continue;
 			indicatorSprite->setScale(4.f);
 		}
 	}

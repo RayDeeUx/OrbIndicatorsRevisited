e4e7407fadc47549683a2e44f29c93cb75e6b184
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-08 22:10:52 -0400

    ok boys time to retire the gag

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Geode/modify/GJBaseGameLayer.hpp>
+/*
 #include <Geode/ui/GeodeUI.hpp>
 #include <random>
 #include <ctime>
+*/
 
 #define NOT_RED gameObject->m_objectType != GameObjectType::RedJumpRing
 #define NOT_PINK gameObject->m_objectType != GameObjectType::PinkJumpRing
@@ -47,6 +49,7 @@ $on_mod(Loaded) {
 	});
 }
 
+/*
 $execute {
 	new EventListener<EventFilter<ModLogoUIEvent>>(+[](ModLogoUIEvent* event) {
 		std::filesystem::path nwo5LogoPath = Mod::get()->getResourcesDir() / "nwo5.png";
@@ -71,3 +74,4 @@ $execute {
 		return ListenerResult::Propagate;
 	});
 }
+*/

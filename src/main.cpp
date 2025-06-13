9b45f9e6e9c59682b5f01367aec8b695e60d8ad8
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-06-13 17:09:57 -0400

    nope nevermind i dont like that

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ class $modify(MyGJBaseGameLayer, GJBaseGameLayer) {
 			if (NON_INDICATOR_ORB) continue;
 
 			const int gameObjectRotation = gameObject->getRotation();
-			if (gameObjectRotation % 180 != 0) indicatorSprite->setRotation(rotation - gameObjectRotation);
+			indicatorSprite->setRotation(rotation - gameObjectRotation);
 
 			const bool flipY = gameObject->isFlipY();
 			const bool flipX = gameObject->isFlipX();
